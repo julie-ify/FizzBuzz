b63d019e69a1534309a0c8e489b1018e22fff5d2
Juliana Ifionu <julieifionu@gmail.com>
2021-09-14 08:26:17 +0100

    Solution

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,18 @@
 module.exports = function () {
   // write your code here
-}
+  const fizzArray = [];
+
+  for (let i = 1; i < 101; i += 1) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      fizzArray.push('FizzBuzz');
+    } else if (i % 5 === 0) {
+      fizzArray.push('Buzz');
+    } else if (i % 3 === 0) {
+      fizzArray.push('Fizz');
+    } else {
+      fizzArray.push(i);
+    }
+  }
+
+  return fizzArray;
+};
