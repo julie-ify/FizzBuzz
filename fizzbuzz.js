732f0a665e7b4aa70d2925c0d4209f07bfea3316
Juliana Ifionu <julieifionu@gmail.com>
2022-01-27 18:18:52 +0100

    Add solution

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,22 @@
 module.exports = function () {
   // write your code here
+
+  let fizzBuzz = [];
+
+  for(let i = 1; i < 101; i ++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+      fizzBuzz.push("FizzBuzz");
+    }
+    else if(i % 5 === 0) {
+      fizzBuzz.push("Buzz");
+    }
+    else if(i % 3 === 0) {
+      fizzBuzz.push("Fizz");
+    }
+    else {
+      fizzBuzz.push(i)
+    }
+  }
+
+  return fizzBuzz;
 }
